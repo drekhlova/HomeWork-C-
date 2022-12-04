c8e82d1a3976685bcbae7271ab92e72b3f5b3347
Daria Rekhlova <dolly1993@yandex.ru>
2022-12-04 21:19:01 +0400

    add lesson2_task15

---
+++ lesson2_task15/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day == 1)
+{
+    string str = day.ToString();
+    Console.WriteLine("Нет"); 
+}
+else if (day == 2)
+{
+    string str = day.ToString();
+    Console.WriteLine("Нет"); 
+}
+else if (day == 3)
+{
+    string str = day.ToString();
+    Console.WriteLine("Нет"); 
+}
+else if (day == 4)
+{
+    string str = day.ToString();
+    Console.WriteLine("Нет"); 
+}
+else if (day == 5)
+{
+    string str = day.ToString();
+    Console.WriteLine("Нет"); 
+}
+else if (day == 6)
+{
+    string str = day.ToString();
+    Console.WriteLine("Да"); 
+}
+else if (day == 7)
+{
+    string str = day.ToString();
+    Console.WriteLine("Да"); 
+}
+else
+{
+    string str = day.ToString();
+    Console.WriteLine("В неделе 7 дней. Пожалуйста, введите число от 1 до 7");
+}  
