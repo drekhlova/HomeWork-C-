24f88e047e44648eec08c991d79eaaf50d3bd5dc
Daria Rekhlova <dolly1993@yandex.ru>
2022-12-08 23:05:28 +0400

    add lesson3_task23

---
+++ lesson3_task23/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 23. Напишите программу, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125 */
+
+/*Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+double GetQuarter(int number)
+{
+    double result = 0;
+    result = (Math.Pow(number, 2));
+    return result;
+}
+Console.WriteLine($"Квадрат числа {number} составит  {GetQuarter(number)}"); */
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 1; i < number+1; i++)
+{
+    Console.WriteLine($"{i*i*i}");
+}
