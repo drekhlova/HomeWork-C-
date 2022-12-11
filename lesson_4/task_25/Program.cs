c3bbb6fd6a3d308edef30357d4b54b1a6bc6044d
Daria Rekhlova <dolly1993@yandex.ru>
2022-12-11 16:53:16 +0400

    add lesson_4

---
+++ lesson_4/task_25/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+/*
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+{
+    double result = 0;
+    result = Math.Pow(A, B);
+    Console.WriteLine(result);
+}
+*/
+
+/*
+Main(A, B);
+void Main(int A, int B)
+{
+    int result = 1;
+    for (int i = 1; i <= B; i++)
+    {
+        result = result * A;
+    }
+    Console.WriteLine($"Результат возведения числа {A} в степень {B} = {result}.");
+}
+*/
+
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+for (int i = 1; i <= B; i++)
+    {
+    result = result * A;
+    }
+Console.WriteLine($"Результат возведения числа {A} в натуральную степень {B} = {result}.");
---
+++ lesson_4/task_29/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+//пользователь сам вводит кол-во элементов и указывает эти элементы
+
+/*
+Console.Write("Введите количество элементов массива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+int[] array =  new int[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}:\t");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Вывод массива: ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write(array[i] + " ");
+    }
+
+    */
+
+int[] number =  new int[8];
+Random random = new Random();
+
+for (int i = 0; i < number.Length; i++)
+{
+    number[i] = random.Next(0, 100);
+}
+for (int i = 0; i < number.Length; i++)
+{
+    Console.Write(number[i] + " ");
+}
